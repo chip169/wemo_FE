50c0840232cfd1014e84da1e418bb436367fb51c
kim xuan hieu <hieukxhe186118@fpt.edu.vn>
2026-06-16 14:01:52 +0700

    Update Deploy BE

--- backend/server.js
+++ backend/server.js
@@ -84,6 +84,7 @@ app.get("/api/support/stream", (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+  res.setHeader("X-Accel-Buffering", "no");
   res.flushHeaders();
 
   if (isAdmin === "true") {
