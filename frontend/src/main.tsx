f04053370e84b68d1ecee6611450fc22d79a0f0c
kim xuan hieu <hieukxhe186118@fpt.edu.vn>
2026-06-19 22:48:30 +0700

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import { Analytics } from "@vercel/analytics/react";
-import ReactGA from "react-ga4";
-
-ReactGA.initialize("G-RTYVMCQK6E");
 
 createRoot(document.getElementById("root")!).render(
   <>
